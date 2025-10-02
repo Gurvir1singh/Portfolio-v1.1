528f3189990a9d6e3331dade78d71a948cf938be
Gurvir Singh <try.gurveer+github1@gmail.com>
2025-10-02 23:38:42 +0530

    resposive work done

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('main'),
-//     smooth: true,
-//     lerp:.05,
-// });
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('main'),
+    smooth: true,
+    lerp:.05,
+});
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector("#line-warper");
+const navLinks = document.querySelector(".nav-links");
+
+function navtoggle(){
+  
+    menu.classList.toggle("menu");
+    menu.classList.toggle("close")
+    console.log("hello");
+    navLinks.classList.toggle("mobile-view");
+    
+    
+}
